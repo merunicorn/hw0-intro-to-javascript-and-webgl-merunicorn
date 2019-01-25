42e8624a3230c7aaa51236df318752a7b3fb2572
Amy Chen <7achen@gmail.com>
2019-01-24 20:21:47 -0500

    Base code w color, stats-js error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import {vec3} from 'gl-matrix';
-import * as Stats from 'stats-js';
+import {vec3, vec4} from 'gl-matrix';
+//import * as Stats from 'stats-js';
+import Stats = require("stats.js");
+console.log(Stats)
 import * as DAT from 'dat-gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
@@ -13,11 +15,14 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  color: [1,0,0],
+  customShader: false,
 };
 
 let icosphere: Icosphere;
 let square: Square;
 let prevTesselations: number = 5;
+let prevColor: number[] = [1,0,0];
 
 function loadScene() {
   icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
@@ -39,6 +44,8 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  gui.addColor(controls, 'color');
+  gui.add(controls, 'custom shader');
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -64,6 +71,11 @@ function main() {
     new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
   ]);
 
+  const custom = new ShaderProgram([
+    new Shader(gl.VERTEX_SHADER, require('./shaders/lambert-vert.glsl')),
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
+  ]);
+
   // This function will be called every frame
   function tick() {
     camera.update();
@@ -76,11 +88,15 @@ function main() {
       icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
       icosphere.create();
     }
+    if(controls.color != prevColor) {
+      prevColor = controls.color;
+    }
     renderer.render(camera, lambert, [
-      icosphere,
-      // square,
-    ]);
-    stats.end();
+        icosphere,
+        // square,
+      ], vec4.fromValues(prevColor[0],prevColor[1],prevColor[2],1));
+      stats.end();
+    }
 
     // Tell the browser to call `tick` again whenever it renders a new frame
     requestAnimationFrame(tick);
